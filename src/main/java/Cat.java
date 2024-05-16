2ddc1a1fb7fc4466fb7b007015b8d146a23b636c
AlbertKhu <x_albert@mail.ru>
2024-05-16 22:26:41 +0300

    first commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Cat(String message) {
+        this.message = message;
+    }
+}
